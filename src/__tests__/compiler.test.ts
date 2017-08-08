2bb86695479ed7d2b96be4e0ceb549316a53e910
Kenju Wagatsuma <ken901waga@gmail.com>
2017-08-08 16:33:10 +0900

    use xit to define pending tests

---
+++ src/__tests__/compiler.test.ts
@@ -0,0 +1,7 @@
+import { compile } from '../compiler';
+
+describe('compile()', () => {
+  xit('compile "Paper 0 Pen 100 Line 0 50 100 50" to SVG HTML Tag', () => {
+    // TODO:
+  });
+});
---
+++ src/__tests__/generator.test.ts
@@ -0,0 +1,7 @@
+import { generator } from '../generator';
+
+describe('generator()', () => {
+  xit('generate "Paper 0 Pen 100 Line 0 50 100 50" SVG HTML tag', () => {
+    // TODO:
+  });
+});
