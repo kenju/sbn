dbbe7b9218d5ee923a66224f3716145f95f51718
Kenju Wagatsuma <ken901waga@gmail.com>
2017-08-08 15:45:09 +0900

    implement parser

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -1,77 +1,7 @@
 import { parser } from '../parser';
 
 describe('parser()', () => {
-  it('parse Paper 0', () => {
-    const expected = {
-      type: "Drawing",
-      body: [{
-        type: "CallExpression",
-        name: "Paper",
-        arguments: [
-          {
-            type: "NumberLiteral",
-            value: 0,
-          },
-        ]
-      }]
-    };
-    const tokens = [
-      { type: "word", value: "Paper" },
-      { type: "number", value: 0 },
-    ];
-    const actual = parser(tokens);
-    expect(actual).toEqual(expected);
-  })
-
-  it('parse Pen 100', () => {
-    const expected = {
-      type: "Drawing",
-      body: [{
-        type: "CallExpression",
-        name: "Pen",
-        arguments: [
-          {
-            type: "NumberLiteral",
-            value: 100,
-          },
-        ]
-      }]
-    };
-    const tokens = [
-      { type: "word", value: "Pen" },
-      { type: "number", value: 100 },
-    ];
-    const actual = parser(tokens);
-    expect(actual).toEqual(expected);
-  })
-
-  it('parse Line 0 50 100 50', () => {
-    const expected = {
-      type: "Drawing",
-      body: [{
-        type: "CallExpression",
-        name: "Line",
-        arguments: [
-          { type: "NumberLiteral", value: 0, },
-          { type: "NumberLiteral", value: 50, },
-          { type: "NumberLiteral", value: 100, },
-          { type: "NumberLiteral", value: 50, },
-        ]
-      }]
-    };
-    const tokens = [
-      { type: "word", value: "Line" },
-      { type: "number", value: 0 },
-      { type: "number", value: 50 },
-      { type: "number", value: 100 },
-      { type: "number", value: 50 },
-    ];
-    const actual = parser(tokens);
-    expect(actual).toEqual(expected);
-  })
-
-  // TODO: remove other unit tests
-  it('parse all', () => {
+  it('parse "Paper 0 Pen 100 Line 0 50 100 50" tokens', () => {
     const expected = {
       type: "Drawing",
       body: [
--- src/parser.ts
+++ src/parser.ts
@@ -1,3 +1,38 @@
-export function parser(tokens) {
+export interface AST {
+  type: string;
+  body: any[];
+};
 
+export function parser(tokens): AST {
+  const body = tokens.reduce((accum, token) => {
+    if (token.type === 'word') {
+      switch (token.value) {
+        case 'Paper':
+        case 'Pen':
+        case 'Line':
+          accum.push({
+            type: 'CallExpression',
+            name: token.value,
+            arguments: [],
+          });
+          return accum;
+        default:
+          throw new Error(`${token.value} is not supported token value.`);
+      }
+    } else if (token.type === 'number') {
+      const lastExpression = accum[accum.length - 1];
+      lastExpression.arguments.push({
+        type: "NumberLiteral",
+        value: token.value,
+      });
+      return accum;
+    } else {
+      throw new Error(`${token.type} is not supported token type.`);
+    }
+  }, []);
+
+  return {
+    type: 'Drawing',
+    body: body
+  };
 }
