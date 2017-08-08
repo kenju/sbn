6d3aba218d71a11f6ed0d180cda07b5ecd7b8537
Kenju Wagatsuma <ken901waga@gmail.com>
2017-08-08 16:58:44 +0900

    implement generator

--- src/__tests__/generator.test.ts
+++ src/__tests__/generator.test.ts
@@ -1,7 +1,46 @@
 import { generator } from '../generator';
 
 describe('generator()', () => {
-  xit('generate "Paper 0 Pen 100 Line 0 50 100 50" SVG HTML tag', () => {
-    // TODO:
+  it('generate "Paper 0 Pen 100 Line 0 50 100 50" SVG HTML tag', () => {
+    const expected = '<svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" version="1.1"><rect x="0" y="0" width="100" height="100" fill="rgb(100%, 100%, 100%)"></rect><line x1="0" y1="50" x2="100" y2="50" stroke="rgb(0%, 0%, 0%)" stroke-linecap="round"></line></svg>';
+
+    const svg_ast = {
+      tag: "svg",
+      attr: {
+        width: 100,
+        height: 100,
+        viewBox: "0 0 100 100",
+        xmlns: "http://www.w3.org/2000/svg",
+        version: "1.1"
+      },
+      body: [
+        {
+          tag: "rect",
+          attr: {
+            x: 0,
+            y: 0,
+            width: 100,
+            height: 100,
+            fill: "rgb(100%, 100%, 100%)"
+          },
+          body: []
+        },
+        {
+          tag: "line",
+          attr: {
+            x1: 0,
+            y1: 50,
+            x2: 100,
+            y2: 50,
+            stroke: "rgb(0%, 0%, 0%)",
+            "stroke-linecap": "round"
+          },
+          body: []
+        }
+      ]
+    };
+
+    const actual = generator(svg_ast);
+    expect(actual).toEqual(expected);
   });
 });
--- src/generator.ts
+++ src/generator.ts
@@ -1,3 +1,17 @@
-export function generator(svgAst) {
+import { SvgAST } from './transformer';
 
+export function generator(svgAst: SvgAST): string {
+  const createAttributes = (attr) => {
+    return Object.keys(attr).map((key) => {
+      return `${key}="${attr[key]}"`;
+    }).join(' ');
+  };
+
+  const attributes = createAttributes(svgAst.attr);
+
+  const elements = svgAst.body.map((node) => {
+    return `<${node.tag} ${createAttributes(node.attr)}></${node.tag}>`;
+  }).join('');
+
+  return `<svg ${attributes}>${elements}</svg>`;
 }
